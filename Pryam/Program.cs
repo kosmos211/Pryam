6580987d4c71b4482a36869afc8340c2be83287c
Константин Иванов <kostet211@mail.ru>
2018-01-19 20:23:41 +0300

    Добавьте файлы проекта.

---
+++ Pryam/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pryam
+{
+    public class Program
+    {
+        public static int Plo(int a, int b)
+        {
+            int S;
+            S = a * b;
+            if (a < 0 || b < 0)
+            {
+                Console.WriteLine("error");
+
+                return 0;
+            }
+
+            else
+            {
+                Console.WriteLine("true, S=", S);
+            }
+            return a * b;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Plo(2, 5));
+            Console.WriteLine(Plo(-3, 2));
+            Console.WriteLine(Plo(2, -5));
+            Console.WriteLine(Plo(-3, -2));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Pryam;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestPlo()
+        {
+            int s = Program.Plo(2, 5);
+
+            Assert.AreEqual<int>(10, s);
+
+
+        }
+
+        [TestMethod]
+        public void TestPlo0()
+        {
+
+            int s = Program.Plo(5, 5);
+
+            Assert.AreEqual<int>(25, s);
+
+
+        }
+
+        [TestMethod]
+        public void TestPlo1()
+        {
+
+            int s = Program.Plo(7, 3);
+
+            Assert.AreEqual<int>(21, s);
+
+
+        }
+
+        [TestMethod]
+        public void TestPlo2()
+        {
+
+            int s = Program.Plo(8, 8);
+
+            Assert.AreEqual<int>(64, s);
+
+
+        }
+
+        [TestMethod]
+        public void TestPlo3()
+        {
+
+            int s = Program.Plo(9, 3);
+
+            Assert.AreEqual<int>(27, s);
+
+
+        }
+
+        [TestMethod]
+        public void TestPlo4()
+        {
+
+            int s = Program.Plo(25, 4);
+
+            Assert.AreEqual<int>(100, s);
+
+
+        }
+
+        [TestMethod]
+        public void TestPlo5()
+        {
+
+            int s = Program.Plo(3, 4);
+
+            Assert.AreEqual<int>(12, s);
+
+        }
+    }
+}
